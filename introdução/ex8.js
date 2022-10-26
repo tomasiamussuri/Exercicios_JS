a757e7d15a46be1c28efc536ee52bed5ad4ce2cf
tomasia.mussuri <tomasia.mussuri@gmail.com>
2022-10-25 22:01:43 -0300

    exercicies

---
+++ introdução/ex8.js
@@ -0,0 +1,6 @@
+var velocidade = 70
+console.log(`A velocidade do seu carro é ${velocidade} km/h.`)
+if (velocidade > 30) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança.')
